384805a88d0bd11568bbfbc54b7a3e3f61414e2a
Sergei <qaserge@gmail.com>
2024-02-08 15:20:04 -0500

    Used composition to make a reusable Box

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import tempMovieData from "./data/TempMovieData";
 import Logo from "./navbar/Logo";
 import Search from "./navbar/Search";
 import NumResults from "./navbar/NumResults";
-import ListBox from "./main/list_box/ListBox";
-import WatchedBox from "./main/watched_box/WatchedBox";
 import MovieList from "./main/list_box/movie_list/MovieList";
+import Box from "./main/Box";
+import WatchedSummary from "./main/watched_box/WatchedSummary";
+import WatchedMoviesList from "./main/watched_box/watched_movie_list/WatchedMoviesList";
+import tempWatchedData from "./data/TempWatchedData";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -19,11 +22,16 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </NavBar>
+
       <Main>
-        <ListBox movies={movies} setMovies={setMovies}>
+        <Box movies={movies} setMovies={setMovies}>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );
