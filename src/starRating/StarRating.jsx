7a7fea96d2981e5cf02ef7b6172c8f2cdbcfe2ac
Sergei <qaserge@gmail.com>
2024-02-20 14:00:50 -0500

    Star component crated

--- src/starRating/StarRating.jsx
+++ src/starRating/StarRating.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import Star from "./Star";
+
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -6,23 +9,24 @@ const containerStyle = {
 
 const starRatingStyle = {
   display: "flex",
-  gap: "4px",
 };
 
 const textStyle = {
-  lineHeight: "1.5",
+  lineHeight: "1",
   margin: "0",
 };
 
 export default function StarRating({ maxRating = 5 }) {
+  const [rating, setRating] = useState(0);
+
   return (
     <div style={containerStyle}>
       <div style={starRatingStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span className="star-rating">★{i + 1}</span>
+          <Star key={i} />
         ))}
       </div>
-      <p style={textStyle}>10</p>
+      <p style={textStyle}>{rating || ""}</p>
     </div>
   );
 }
