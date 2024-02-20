820117df91e6a4017c2995d9100c894772aa6025
Sergei <qaserge@gmail.com>
2024-02-20 14:18:18 -0500

    Star click functionality implemented

--- src/starRating/StarRating.jsx
+++ src/starRating/StarRating.jsx
@@ -17,13 +17,21 @@ const textStyle = {
 };
 
 export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(1);
+
+  function handleRating(rating) {
+    setRating(rating);
+  }
 
   return (
     <div style={containerStyle}>
       <div style={starRatingStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} />
+          <Star
+            key={i}
+            onRate={() => handleRating(i + 1)}
+            full={rating >= i + 1}
+          />
         ))}
       </div>
       <p style={textStyle}>{rating || ""}</p>
