4ce29f1e93919181f0e451537e1c37e10a8dcccb
Sergei <qaserge@gmail.com>
2024-04-22 13:27:59 -0400

    Stars Improving Reusability With Props. Added onSetRating prop handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./starRating/StarRating";
 // import './index.css';
@@ -9,9 +9,25 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating color={"red"} size={30} className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="green" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/starRating/StarRating.jsx
+++ src/starRating/StarRating.jsx
@@ -11,21 +11,34 @@ const starRatingStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [hoverRating, setHoverRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    if (onSetRating) {
+      onSetRating(rating);
+    }
   }
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 2}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className="className">
       <div style={starRatingStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -34,10 +47,16 @@ export default function StarRating({ maxRating = 5 }) {
             full={hoverRating ? hoverRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setHoverRating(i + 1)}
             onHoverOut={() => setHoverRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{hoverRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[hoverRating ? hoverRating - 1 : rating - 1]
+          : hoverRating || rating || ""}
+      </p>
     </div>
   );
 }
