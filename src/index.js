36c1a1ccb10ff4b44e8880ca59e1b94d7e718e1f
Sergei <qaserge@gmail.com>
2024-04-25 14:52:37 -0400

    TextExpander component added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./starRating/StarRating";
+import TextExpander from "./textExpander/TextExpander";
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -15,6 +16,21 @@ root.render(
     />
     <StarRating color={"red"} size={30} className="test" defaultRating={3} />
     <Test />
+
+    <TextExpander
+      collapsedNumWords={20}
+      expandButtonText="Show text"
+      collapseButtonText="Collapse text"
+      buttonColor="#ff6622"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </TextExpander>
   </React.StrictMode>
 );
 
---
+++ src/textExpander/TextExpander.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function TextExpander({
+  collapsedNumWords = 10,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "#1f09cd",
+  expanded = false,
+  className,
+  children,
+}) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+
+  let displayText = children;
+
+  if (typeof children === "string") {
+    displayText = isExpanded
+      ? children
+      : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+  }
+
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor,
+  };
+
+  return (
+    <div className={className}>
+      <span>{String(displayText)}</span>
+      <button onClick={() => setIsExpanded((exp) => !exp)} style={buttonStyle}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
+}
