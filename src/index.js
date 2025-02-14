ae49a9b9019b4c14f882f6644a15accd786d08ef
Sergei <qaserge@gmail.com>
2025-02-14 15:20:48 -0500

    Fetch API added. Custom hooks added

--- src/index.js
+++ src/index.js
@@ -1,51 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./starRating/StarRating";
-import TextExpander from "./textExpander/TextExpander";
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
-      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating color={"red"} size={30} className="test" defaultRating={3} />
-    <Test />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
 
-    <TextExpander
-      collapsedNumWords={20}
-      expandButtonText="Show text"
-      collapseButtonText="Collapse text"
-      buttonColor="#ff6622"
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </TextExpander>
+    <Test /> */}
   </React.StrictMode>
 );
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="green" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
